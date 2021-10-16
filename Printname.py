50a30b9ccc9529a68ffe0984a0c1044efbf6cf75
SeanHarveyOfiangga <seanharveyofiangga@gmail.com>
2021-10-16 18:50:50 +0800

    First commit

---
+++ Printname.py
@@ -0,0 +1,1 @@
+print("Sean Harvey S. Ofiangga")
---
+++ test.py
@@ -0,0 +1,5 @@
+""" Basic Addition Calculator """
+a = int(input("Enter your first number: "))
+b = int(input("Enter your second number: "))
+result = a + b
+print(f"{a} + {b} = {result}")
